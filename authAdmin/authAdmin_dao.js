120453b83307e436fb68e14bc13a7021c960c67a
Carlos Sepulveda <sepul0392@unicauca.edu.co>
2019-11-14 22:16:00 -0500

    Creacion Admin y configuracion, arreglo loginDocente para cambiar correo por nombre_usuario

---
+++ authAdmin/authAdmin_dao.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const adminSchema = require('./authAdmin_model');
+
+adminSchema.statics={
+    create: function(data, cb){
+        const admin = new this(data);
+        admin.save(cb);
+    },
+    login: function(query,cb){
+        this.find(query,cb);
+    }
+}
+const adminModel = mongoose.model('Admin',adminSchema);
+
+module.exports = adminModel;
---
+++ authAdmin/authAdmin_model.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.set('useCreateIndex',true);
+mongoose.set('useUnifiedTopology',true);
+const adminSchema = new Schema({
+    id_admin:{
+        type: Number,
+        required: true,
+        trim: true,
+    },
+    tipo_usuario:{
+        type: Number,
+        required: true,
+        trim: true,
+    },
+    id_colegio:{
+        type: Number,
+        trim: true,
+    },
+    nombre_usuario:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    contrasena:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    //id_admin	tipo_usuario	
+    //id_colegio	
+    //nombre_usuario	contraseña	
+
+},{timestamps: true});
+
+module.exports = adminSchema;
